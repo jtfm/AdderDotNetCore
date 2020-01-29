46c1c2123caf85b9af82c344b0247e65fdfce893
Jack Milner <jack.milner@bigupdata.co.uk>
2020-01-29 21:23:40 +0000

    Added calling code for Adder to the Program class.

--- AdderDotNetCore/Program.cs
+++ AdderDotNetCore/Program.cs
@@ -7,6 +7,11 @@ namespace AdderDotNetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int x = 2;
+            int y = 2;
+
+            Console.WriteLine($"{x} + {y} = {Adder.Add(x, y)}");
         }
     }
 }
